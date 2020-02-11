4ebba0cc4280152f41be596de3d212b3fe72bf45
Benjamin Dickins <ben.dickins@cantab.net>
2020-02-11 15:01:02 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=thinking
+#SBATCH -N 1 -c 2 --mem=16G
+#SBATCH --time=0-0:1:0
+#SBATCH --mail-user=EMAIL HERE
+#SBATCH --mail-type=ALL
+#SBATCH --output=/users/bio3dickib/azure_up_%j.log
+
+pwd; hostname; date
+echo "Starting job"
+echo "I am thinking"
+echo "Finished job"
+date
